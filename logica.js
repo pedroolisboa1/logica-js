4b1be6fbd26604d357562279bcf0005516ea33a7
pedroolisboa1 <41169379+pedroolisboa1@users.noreply.github.com>
2024-01-17 16:15:54 -0300

    Create logica.js
    Estudo de lógica e programação em javascript
    Desafio dos 10 exercícios

---
+++ logica.js
@@ -0,0 +1,20 @@
+alert('Boas vindas ao nosso site!');
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos.');
+
+let mensagemDeErro = "Erro! Preencha todos os campos (mensagemDeErro)";
+alert(mensagemDeErro);
+
+let nome1 = prompt('Qual o seu nome de usuario?')
+
+let idade1 = prompt('Qual a sua idade?')
+habilitacao = 18
+if (idade1 >= habilitacao) {
+    alert('Pode tirar a habilitação!')
+}
+
